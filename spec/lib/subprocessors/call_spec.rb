0cd82ed4c7655ade878c59bd7c06d5702d80e7f9
Jared Beck <jared@jaredbeck.com>
2015-03-29 18:09:24 -0400

    Reduce duplication in tests

--- spec/lib/subprocessors/call_spec.rb
+++ spec/lib/subprocessors/call_spec.rb
@@ -4,18 +4,25 @@ module MinitestToRspec
   module Subprocessors
     RSpec.describe Call do
       describe "#process_call" do
-        it "replaces test_helper with spec_helper" do
-          input = s(:call, nil, :require, s(:str, "test_helper"))
-          expect(described_class.process(input)).to eq(
-            s(:call, nil, :require, s(:str, "spec_helper"))
-          )
+
+        # Returns an S-expression representing a method call.
+        def exp(method_name, argument)
+          s(:call, nil, method_name.to_sym, s(:str, argument.to_s))
         end
 
-        it "replaces test with it" do
-          input = s(:call, nil, :test, s(:str, "is delicious"))
-          expect(described_class.process(input)).to eq(
-            s(:call, nil, :it, s(:str, "is delicious"))
-          )
+        def process(input)
+          described_class.process(input)
+        end
+
+        it "replaces `test_helper` with `spec_helper`" do
+          input = exp(:require, "test_helper")
+          expect(process(input)).to eq(exp(:require, "spec_helper"))
+        end
+
+        it "replaces `test` with `it`" do
+          argument = "is delicious"
+          input = exp(:test, argument)
+          expect(process(input)).to eq(exp(:it, argument))
         end
       end
     end

27d1c8f4ca7911bfcd0a786f034a5c2798bac39b
Jared Beck <jared@jaredbeck.com>
2015-04-02 13:00:22 -0400

    Organize

--- lib/minitest_to_rspec/subprocessors/call.rb
+++ lib/minitest_to_rspec/subprocessors/call.rb
@@ -1,63 +1,11 @@
+require_relative '../exp/call'
+
 module MinitestToRspec
   module Subprocessors
-
-    # Processes `:call` s-expressions
     class Call
-
-      # Represents a `:call` s-expression
-      class Exp
-
-        ASSERTIONS = %i[
-          assert
-          assert_equal
-          refute
-          refute_equal
-        ]
-
-        attr_reader :original
-
-        def initialize(exp)
-          raise ArgumentError unless exp.sexp_type == :call
-          @exp = exp.dup
-          @original = exp.dup
-        end
-
-        def arguments
-          @exp[3..-1]
-        end
-
-        def assertion?
-          ASSERTIONS.include?(method_name)
-        end
-
-        def method_name
-          @exp[2]
-        end
-
-        def one_string_argument?
-          arguments.length == 1 && string?(arguments[0])
-        end
-
-        def require_test_helper?
-          method_name == :require &&
-            one_string_argument? &&
-            arguments[0][1] == "test_helper"
-        end
-
-        def receiver
-          @exp[1]
-        end
-
-        private
-
-        def string?(exp)
-          exp.sexp_type == :str
-        end
-      end
-
       class << self
         def process(sexp)
-          exp = Exp.new(sexp)
+          exp = Exp::Call.new(sexp)
           sexp.clear
           if exp.require_test_helper?
             require_spec_helper

e7157ecf3a5b1e10cf4c0e575dd96f0133f0627a
Jared Beck <jared@jaredbeck.com>
2015-04-02 16:38:43 -0400

    Do not run codeclimate-test-reporter locally

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV.key?('CODECLIMATE_REPO_TOKEN')
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest_to_rspec'

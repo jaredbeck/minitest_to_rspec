0ff29c9afd33fd9a60b018db61b0c5ad49a76c60
Jared Beck <jared@jaredbeck.com>
2015-04-18 19:54:37 -0400

    Add example of Base.allow_to

--- spec/lib/subprocessors/base_spec.rb
+++ spec/lib/subprocessors/base_spec.rb
@@ -3,7 +3,21 @@ require "spec_helper"
 module MinitestToRspec
   module Subprocessors
     RSpec.describe Base do
-      # TODO
+      describe ".allow_to" do
+        let(:msg_recipient) { double }
+        let(:matcher) { double }
+
+        it "returns a call expression" do
+          exp = described_class.allow_to(msg_recipient, matcher)
+          expect(exp).to eq(
+            s(:call,
+              s(:call, nil, :allow, msg_recipient),
+              :to,
+              matcher
+            )
+          )
+        end
+      end
     end
   end
 end

1f6b7ee0a41af49cd66874d272b34a9ba7eae6af
Jared Beck <jared@jaredbeck.com>
2015-04-18 19:29:05 -0400

    A simple way to run simplecov locally

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 if ENV.key?('CODECLIMATE_REPO_TOKEN')
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
+elsif ENV.key?('START_SIMPLECOV')
+  require 'simplecov'
+  SimpleCov.start
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)

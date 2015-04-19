493aa373f7459ea68d48608afca8619eb5428d48
Jared Beck <jared@jaredbeck.com>
2015-04-19 18:03:08 -0400

    Fix ruby 2.0 compatability issue

--- lib/minitest_to_rspec/exp/hash_exp.rb
+++ lib/minitest_to_rspec/exp/hash_exp.rb
@@ -8,8 +8,11 @@ module MinitestToRspec
         @exp = sexp.dup
       end
 
+      # A slightly nicer implementation would be:
+      # `@exp[1..-1].each_slice(2).to_h`
+      # but that would require ruby >= 2.1
       def to_h
-        @exp[1..-1].each_slice(2).to_h
+        Hash[@exp[1..-1].each_slice(2).to_a]
       end
     end
   end

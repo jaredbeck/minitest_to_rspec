25d15a39bc50cbad7f34e6ac8329acde10a7db95
Jared Beck <jared@jaredbeck.com>
2015-05-11 18:01:43 -0400

    Organize by method name, and alphabetize

--- spec/lib/subprocessors/call_spec.rb
+++ spec/lib/subprocessors/call_spec.rb
@@ -18,47 +18,14 @@ module MinitestToRspec
         described_class.new(input, rails).process
       end
 
-      it "does not replace unknown requires" do
-        input = -> { parse("require 'a_shrubbery'") }
-        expect(process(input.call)).to eq(input.call)
-      end
-
-      describe "rails option" do
-        context "option is false" do
-          it "replaces test_helper with spec_helper" do
-            input = exp(:require, "test_helper")
-            expect(process(input, false)).to eq(exp(:require, "spec_helper"))
-          end
+      context "assert" do
+        it "replaces `assert` with `expect` to be truthy" do
+          expect(
+            process(parse("assert Banana.new.delicious?"))
+          ).to eq(
+            parse("expect(Banana.new.delicious?).to be_truthy")
+          )
         end
-
-        context "option is true" do
-          it "replaces test_helper with rails_helper" do
-            input = exp(:require, "test_helper")
-            expect(process(input, true)).to eq(exp(:require, "rails_helper"))
-          end
-        end
-      end
-
-      it "replaces `test` with `it`" do
-        argument = "is delicious"
-        input = exp(:test, argument)
-        expect(process(input)).to eq(exp(:it, argument))
-      end
-
-      it "replaces `assert` with `expect` to be truthy" do
-        expect(
-          process(parse("assert Banana.new.delicious?"))
-        ).to eq(
-          parse("expect(Banana.new.delicious?).to be_truthy")
-        )
-      end
-
-      it "replaces `refute` with `expect` to be falsey" do
-        expect(
-          process(parse("refute Kiwi.new.delicious?"))
-        ).to eq(
-          parse("expect(Kiwi.new.delicious?).to be_falsey")
-        )
       end
 
       context "assert_equal" do
@@ -75,6 +42,26 @@ module MinitestToRspec
             process(parse("assert_equal false, Kiwi.new.delicious?, 'asdf'"))
           ).to eq(
             parse("expect(Kiwi.new.delicious?).to eq(false)")
+          )
+        end
+      end
+
+      context "assert_match" do
+        it "replaces assert_match with expect to match" do
+          expect(
+            process(parse('assert_match(/nana\Z/, "banana")'))
+          ).to eq(
+            parse('expect("banana").to match(/nana\Z/)')
+          )
+        end
+      end
+
+      context "assert_nil" do
+        it "replaces assert_nil with expect to be_nil" do
+          expect(
+            process(parse('assert_nil(kiwi)'))
+          ).to eq(
+            parse('expect(kiwi).to(be_nil)')
           )
         end
       end
@@ -97,124 +84,24 @@ module MinitestToRspec
         end
       end
 
-      it "replaces refute_equal with expect to_not eq" do
-        expect(
-          process(parse("refute_equal(true, Kiwi.new.delicious?)"))
-        ).to eq(
-          parse("expect(Kiwi.new.delicious?).to_not eq(true)")
-        )
-      end
-
-      it "replaces assert_match with expect to match" do
-        expect(
-          process(parse('assert_match(/nana\Z/, "banana")'))
-        ).to eq(
-          parse('expect("banana").to match(/nana\Z/)')
-        )
-      end
-
-      it "replaces assert_nil with expect to be_nil" do
-        expect(
-          process(parse('assert_nil(kiwi)'))
-        ).to eq(
-          parse('expect(kiwi).to(be_nil)')
-        )
-      end
-
-      it "does not change factory call" do
-        input = -> {
-          s(:call,
-            nil,
-            :create,
-            s(:lit, :banana),
-            s(:hash,
-              s(:lit, :peel),
-              s(:call, nil, :peel),
-              s(:lit, :color),
-              s(:str, "yellow"),
-              s(:lit, :delicious),
-              s(:true)
+      context "create" do
+        it "does not change factory call" do
+          input = -> {
+            s(:call,
+              nil,
+              :create,
+              s(:lit, :banana),
+              s(:hash,
+                s(:lit, :peel),
+                s(:call, nil, :peel),
+                s(:lit, :color),
+                s(:str, "yellow"),
+                s(:lit, :delicious),
+                s(:true)
+              )
             )
-          )
-        }
-        expect(process(input.call)).to eq(input.call)
-      end
-
-      context "returns" do
-        it "replaces stubs/returns with expect to receive" do
-          expect(
-            process(parse("Banana.stubs(:delicious?).returns(true)"))
-          ).to eq(
-            parse("allow(Banana).to receive(:delicious?).and_return(true)")
-          )
-        end
-
-        it "converts any_instance.expects" do
-          expect(
-            process(parse("Banana.any_instance.stubs(:delicious?).returns(true)"))
-          ).to eq(
-            parse("allow_any_instance_of(Banana).to receive(:delicious?).and_return(true)")
-          )
-        end
-
-        it "converts any_instance.stubs" do
-          expect(
-            process(parse("Banana.any_instance.expects(:delicious?).returns(true)"))
-          ).to eq(
-            parse("expect_any_instance_of(Banana).to receive(:delicious?).and_return(true)")
-          )
-        end
-
-        it "does not replace every method named 'returns'" do
-          # In this example, `returns` does not represent a mocha stub.
-          input = -> { parse("Banana.returns(peel)") }
+          }
           expect(process(input.call)).to eq(input.call)
-        end
-      end
-
-      context "stub" do
-        it "replaces stub with double" do
-          expect(process(parse("stub"))).to eq(parse('double'))
-        end
-
-        it "replaces stub(hash) with double" do
-          expect(
-            process(parse("stub(:delicious? => true)"))
-          ).to eq(
-            parse('double(:delicious? => true)')
-          )
-        end
-
-        it "does not replace explicit receiver stub" do
-          input = -> { parse("pencil.stub") }
-          expect(process(input.call)).to eq(input.call)
-        end
-      end
-
-      context "stub_everything" do
-        it "replaces with double as_null_object" do
-          expect(
-            process(parse("stub_everything"))
-          ).to eq(
-            parse('double.as_null_object')
-          )
-        end
-
-        context "with explicit receiver" do
-          it "does not replace" do
-            input = -> { parse("pencil.stub_everything") }
-            expect(process(input.call)).to eq(input.call)
-          end
-        end
-
-        context "with specific allowed methods" do
-          it "replaces with double as_null_object" do
-            expect(
-              process(parse("stub_everything(:delicious? => false)"))
-            ).to eq(
-              parse('double(:delicious? => false).as_null_object')
-            )
-          end
         end
       end
 
@@ -275,6 +162,132 @@ module MinitestToRspec
         end
       end
 
+      context "refute" do
+        it "replaces `refute` with `expect` to be falsey" do
+          expect(
+            process(parse("refute Kiwi.new.delicious?"))
+          ).to eq(
+            parse("expect(Kiwi.new.delicious?).to be_falsey")
+          )
+        end
+      end
+
+      context "refute_equal" do
+        it "replaces refute_equal with expect to_not eq" do
+          expect(
+            process(parse("refute_equal(true, Kiwi.new.delicious?)"))
+          ).to eq(
+            parse("expect(Kiwi.new.delicious?).to_not eq(true)")
+          )
+        end
+      end
+
+      context "returns" do
+        it "replaces stubs/returns with expect to receive" do
+          expect(
+            process(parse("Banana.stubs(:delicious?).returns(true)"))
+          ).to eq(
+            parse("allow(Banana).to receive(:delicious?).and_return(true)")
+          )
+        end
+
+        it "converts any_instance.expects" do
+          expect(
+            process(parse("Banana.any_instance.stubs(:delicious?).returns(true)"))
+          ).to eq(
+            parse("allow_any_instance_of(Banana).to receive(:delicious?).and_return(true)")
+          )
+        end
+
+        it "converts any_instance.stubs" do
+          expect(
+            process(parse("Banana.any_instance.expects(:delicious?).returns(true)"))
+          ).to eq(
+            parse("expect_any_instance_of(Banana).to receive(:delicious?).and_return(true)")
+          )
+        end
+
+        it "does not replace every method named 'returns'" do
+          # In this example, `returns` does not represent a mocha stub.
+          input = -> { parse("Banana.returns(peel)") }
+          expect(process(input.call)).to eq(input.call)
+        end
+      end
+
+      context "require" do
+        it "does not replace unknown requires" do
+          input = -> { parse("require 'a_shrubbery'") }
+          expect(process(input.call)).to eq(input.call)
+        end
+
+        context "rails option is false" do
+          it "replaces test_helper with spec_helper" do
+            input = exp(:require, "test_helper")
+            expect(process(input, false)).to eq(exp(:require, "spec_helper"))
+          end
+        end
+
+        context "rails option is true" do
+          it "replaces test_helper with rails_helper" do
+            input = exp(:require, "test_helper")
+            expect(process(input, true)).to eq(exp(:require, "rails_helper"))
+          end
+        end
+      end
+
+      context "stub" do
+        it "replaces stub with double" do
+          expect(process(parse("stub"))).to eq(parse('double'))
+        end
+
+        it "replaces stub(hash) with double" do
+          expect(
+            process(parse("stub(:delicious? => true)"))
+          ).to eq(
+            parse('double(:delicious? => true)')
+          )
+        end
+
+        it "does not replace explicit receiver stub" do
+          input = -> { parse("pencil.stub") }
+          expect(process(input.call)).to eq(input.call)
+        end
+      end
+
+      context "stub_everything" do
+        it "replaces with double as_null_object" do
+          expect(
+            process(parse("stub_everything"))
+          ).to eq(
+            parse('double.as_null_object')
+          )
+        end
+
+        context "with explicit receiver" do
+          it "does not replace" do
+            input = -> { parse("pencil.stub_everything") }
+            expect(process(input.call)).to eq(input.call)
+          end
+        end
+
+        context "with specific allowed methods" do
+          it "replaces with double as_null_object" do
+            expect(
+              process(parse("stub_everything(:delicious? => false)"))
+            ).to eq(
+              parse('double(:delicious? => false).as_null_object')
+            )
+          end
+        end
+      end
+
+      context "test" do
+        it "replaces `test` with `it`" do
+          argument = "is delicious"
+          input = exp(:test, argument)
+          expect(process(input)).to eq(exp(:it, argument))
+        end
+      end
     end
   end
 end

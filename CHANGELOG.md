7cbc9b7f4e581b7a89ed141ababa7fd6b6c8a9f9
Jared Beck <jared@jaredbeck.com>
2017-11-01 23:44:01 -0400

    Add MinitestToRspec.gem_version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,7 +13,7 @@ Unreleased
 
 ### Added
 
-- None
+- MinitestToRspec.gem_version
 
 ### Fixed
 
--- lib/minitest_to_rspec/version.rb
+++ lib/minitest_to_rspec/version.rb
@@ -1,3 +1,8 @@
+#:nodoc:
 module MinitestToRspec
   VERSION = "0.9.0".freeze
+
+  def self.gem_version
+    ::Gem::Version.new(VERSION)
+  end
 end
